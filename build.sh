0d4716bd91d059ec206c37ce10b3cb07c4b1f17c
Frozen <mrozix11@gmail.com>
2018-09-19 17:59:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ cd openocd
 
 ./bootstrap
 
-./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-linux-gnueabihf
+./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-linux-gnueabihf CC=clang
 
-make CC=gcc-arm-linux-gnueabihf
+make
 
 make install
 
