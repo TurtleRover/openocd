ace94fa0e59ac89c41b0830692f0b44ae35931cc
stsdc <stanislaw.dac@gmail.com>
2018-09-19 16:30:02 +0200

    add CC in build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ cd openocd
 
 ./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-linux-gnueabihf
 
-make
+make CC=arm-linux-gnueabi-gcc
 
 make install
 
