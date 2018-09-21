1eabbae9faebede1cdc58d803b91b26488e5ec3e
Stanisław <stanislaw.dac@gmail.com>
2018-09-21 23:03:12 +0200

    cleanup

--- build.sh
+++ build.sh
@@ -3,12 +3,11 @@
 cd openocd
 
 ./bootstrap
-CC=gcc-arm-linux-gnueabihf
 
-./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-unknown-linux-gnueabihf --target=arm-unknown-linux-gnueabihf
+./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-linux-gnueabihf
 
 make
 
-make install LIBNBIS=no
+make install
 
 cd ..
