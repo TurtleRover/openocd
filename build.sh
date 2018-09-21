896a643b07a711e292fc4cde489b0634e74ed168
Stanisław <stanislaw.dac@gmail.com>
2018-09-21 22:00:15 +0200

    Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Update build.sh Travis conf for Raspberry Pi
    Merge remote-tracking branch 'origin/dev_frozen' into dev_frozen
    
    
    .

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ cd openocd
 
 ./bootstrap
 
-./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=armv7-linux-gnueabihf
+./configure --enable-sysfsgpio --enable-bcm2835gpio --disable-werror --prefix=/tmp/openocd --host=arm-unknown-linux-gnueabihf --target=arm-unknown-linux-gnueabihf
 
 make
 
-make install
+make install LIBNBIS=no
 
 cd ..
