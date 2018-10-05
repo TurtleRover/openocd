51fc0193a61ae9e6b6fdf1d93ee1a723cd34c551
Frozen <mrozix11@gmail.com>
2018-10-05 14:13:34 +0200

    Update after-install.sh

--- after-install.sh
+++ after-install.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 cp /usr/local/share/openocd/contrib/60-openocd.rules /etc/udev/rules.d/
+
+
+echo "#### TURTLE ROVER ALIASES ###" >> /home/pi/.bashrc
+echo "alias turtle-shield-flash='sudo openocd -f /opt/turtlerover/openocd/config/shield.cfg'" >> /home/pi/.bashrc
